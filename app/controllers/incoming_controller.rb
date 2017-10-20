3f88972f2b5de8a9c95c0dc3242b96004fd6e98b
Rory Trunkhill <rtrunkhill@gmail.com>
2017-10-20 22:24:30 +0000

    CL look at me if you'd like

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -6,7 +6,7 @@ class IncomingController < ApplicationController
   def create
     # Take a look at these in your server logs
     # to get a sense of what you're dealing with.
-    # puts "INCOMING PARAMS HERE: #{params}"
+    puts "INCOMING PARAMS HERE: #{params}"
     
     # You put the message-splitting and business
     # magic here.
