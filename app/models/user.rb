4ee3bbef9d5f60f39647825702c5671b71a0e2dd
Rory Trunkhill <rtrunkhill@gmail.com>
2017-10-23 21:50:58 +0000

    whoops this should be in cp8

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
          
-  has_many :topics
+  has_many :topics 
+  has_many :likes,  dependent: :destroy
   
   after_create :send_admin_mail
   
@@ -12,4 +13,7 @@ class User < ActiveRecord::Base
     FavoriteMailer.new_user(self).deliver
   end
 
+  def liked(bookmark)
+    likes.where(bookmark_id: bookmark.id).first
+  end
 end
