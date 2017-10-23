7056aff8444102207bd7af1791c5a8b234fae4b1
Rory Trunkhill <rtrunkhill@gmail.com>
2017-10-23 21:08:10 +0000

    cp7 complete

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171016223401) do
+ActiveRecord::Schema.define(version: 20171023204915) do
 
   create_table "bookmarks", force: :cascade do |t|
     t.string   "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "topic_id"
+    t.integer  "user_id"
   end
 
   add_index "bookmarks", ["topic_id"], name: "index_bookmarks_on_topic_id"
@@ -26,6 +27,7 @@ ActiveRecord::Schema.define(version: 20171016223401) do
     t.string   "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
